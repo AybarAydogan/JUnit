53461ac980b5844eccb4018b796a444f224ef61d
Aybar <you@example.com>
2023-11-23 14:30:16 +0300

    7.Gün

--- src/test/java/practice/P01.java
+++ src/test/java/practice/P01.java
@@ -1,5 +1,39 @@
 package practice;
 
+import org.junit.*;
+
 public class P01 {
 
+    /*
+ @BeforeClass---> Tüm Method'dan önce 1 kere çalışır
+ @AfterClass---> Tüm Method'dan sonra 1 kere çalışır
+ @Before---> Her Method'dan önce  1 kere çalışır
+ @After---> Her Method'dan sonra  1 kere çalışır
+ @Test---> Methodlarımızı çalıştırır
+​
+*/
+    @Test
+    public void ayca01(){
+        System.out.println("ayça");
+    }
+
+    @Test
+    public void nevfel05(){
+        System.out.println("nevfel");
+    }
+
+    @Test
+    public void zeybek(){
+        System.out.println("zeybek");
+    }
+
+    @Test
+    public void samet(){
+        System.out.println("samet");
+    }
+
+    @Test
+    public void zehra(){
+        System.out.println("zehra");
+    }
 }
