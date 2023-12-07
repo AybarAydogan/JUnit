7b604f82b2aa0950749350d07bbca22578f2092f
Aybar <you@example.com>
2023-12-07 11:42:48 +0300

    16.gün

---
+++ src/test/java/practice/P08.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class P08 {
+
+      /*
+//1."https://testotomasyonu.com/webtables" adresine gidin
+//2.Web table tum body'sini yazdirin
+//3. Web tablosunda "Comfortable Gaming Chair" bulundugunu test edin
+//4.Web table'daki satir sayisinin 5 oldugunu test edin
+//5.Tum satirlari yazdirin
+//6. Web table'daki sutun sayisinin 4 olduğunu test edin
+//6. Web table'daki sutun sayisinin 4 olduğunu test edin
+//7. 3.sutunu yazdirin
+//8. Tablodaki basliklari yazdirin
+//10.  4.satirdaki category degerinin "Furniture" oldugunu test edin
+     */
+}
